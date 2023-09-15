eab7152c644801c3db4f495b5333f6e7d54a17ee
Andrew McKinney <andrewryanmckinney@gmail.com>
2023-09-15 15:47:09 -0500

    Update setup.sh

--- setup/bash/setup.sh
+++ setup/bash/setup.sh
@@ -2,6 +2,7 @@ apt-get update
 apt-get install -y sudo curl wget git unzip ruby ruby-dev ruby-colorize build-essential ranger bat neofetch
 
 # ranger config
+mkdir -p ~/.config/ranger
 sudo curl https://raw.githubusercontent.com/armckinney/armckinney/local-add-setup-automation/setup/bash/config/rc.conf -o ~/.config/ranger/rc.conf
 
 # micro install and config
@@ -10,13 +11,13 @@ echo '{"clipboard": "terminal"}' > ~/.config/micro/settings.json
 
 # nerdfont install and setup
 mkdir /usr/share/fonts && cd /usr/share/fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/SourceCodePro.zip
-unzip SourceCodePro.zip -d ~/.fonts
+sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/SourceCodePro.zip
+sudo unzip SourceCodePro.zip -d ~/.fonts
 fc-cache -fv
 
 # oh-my-posh install and setup
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
-chmod +x /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
 mkdir ~/.poshthemes
 sudo curl https://raw.githubusercontent.com/armckinney/armckinney/main/setup/bash/config/armck.omp.json -o ~/.poshthemes/armck.omp.json
 
@@ -24,7 +25,7 @@ sudo curl https://raw.githubusercontent.com/armckinney/armckinney/main/setup/bas
 sudo gem install colorls
 
 # batcat alias
-sudo mkdir -p ~/.local/bin && ln -s /usr/bin/batcat ~/.local/bin/bat
+sudo mkdir -p ~/.local/bin && sudo ln -s /usr/bin/batcat ~/.local/bin/bat
 
 # add template bashrc to user profile
 sudo curl https://raw.githubusercontent.com/armckinney/armckinney/main/setup/bash/config/bashrc.sh >> ~/.bashrc
